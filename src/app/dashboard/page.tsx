46c22f2534b6046bcb3978b98797e2ce258b530b
korasell <elite.academy226@gmail.com>
2026-08-01 16:59:23 +0000

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,12 @@
-export default function DashboardPage() {
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return <h1>Dashboard</h1>;
 }
